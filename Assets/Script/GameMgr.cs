9af37585ccfd4ad35edcc10308ed5bfab55183f6
Muhammad Anwar Safei <muhammad.anwar.safei@gmail.com>
2018-08-16 10:51:05 +0700

    Commit Project
    Commit Project

---
+++ Assets/Script/GameMgr.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour {
+    public GameObject Objek;
+
+    bool rotatedRight;
+    bool rotatedLeft;
+
+    bool isZoomIn;
+    bool isZoomOut;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rotatedRight)
+        {
+            Objek.transform.Rotate(Vector3.up, 100.0f * Time.deltaTime);
+        }
+        else if (rotatedLeft)
+        {
+            Objek.transform.Rotate(Vector3.down, 100.0f * Time.deltaTime);
+        }
+
+        if (isZoomIn)
+        {
+            Objek.transform.localScale += new Vector3(0.0001f, 0.0001f, 0.0001f);
+        }
+        else if (isZoomOut)
+        {
+            Objek.transform.localScale -= new Vector3(0.0001f, 0.0001f, 0.0001f);
+        }
+    }
+
+    public void RotateRightPressed()
+    {
+        rotatedRight = true;
+    }
+
+    public void RotateRightReleased()
+    {
+        rotatedRight = false;
+    }
+
+    public void RotateLeftPressed()
+    {
+        rotatedLeft = true;
+    }
+
+    public void RotateLeftReleased()
+    {
+        rotatedLeft = false;
+    }
+
+    //Zoom
+    public void ZoomINPressed()
+    {
+        isZoomIn = true;
+    }
+    public void ZoomINReleased()
+    {
+        isZoomIn = false;
+    }
+
+    public void ZoomOUTPressed()
+    {
+        isZoomOut = true;
+    }
+    public void ZoomOUTReleased()
+    {
+        isZoomOut = false;
+    }
+
+}
---
+++ Assets/Script/MenuMgr.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimplePopup;
+
+public class MenuMgr : MonoBehaviour {
+
+    public GameObject MainMenu;
+    public GameObject Panduan;
+
+    public AudioSource backsound;
+    public AudioSource backsoundAR;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (MainMenu.activeSelf)
+            {
+                SimplePopupManager.Instance.CreatePopup("Are you sure want to quit?");
+                SimplePopupManager.Instance.AddButton("Yes", delegate { Application.Quit(); });
+                SimplePopupManager.Instance.AddButton("No", delegate { SimplePopupManager.Instance.HidePopup(); });
+                SimplePopupManager.Instance.ShowPopup();
+            }
+            else
+            {
+                MainMenu.SetActive(true);
+                Panduan.SetActive(false);
+
+                if (backsoundAR.isPlaying)
+                {
+                    backsound.Play();
+                    backsoundAR.Stop();
+                }
+            }
+        }
+
+        
+		
+	}
+
+    public void StartAR()
+    {
+        MainMenu.SetActive(false);
+
+        backsoundAR.Play();
+        backsound.Stop();
+    }
+
+    public void OpenPanduan()
+    {
+        MainMenu.SetActive(false);
+        Panduan.SetActive(true);
+    }
+
+    public void BackToMainmenu()
+    {
+        MainMenu.SetActive(true);
+        Panduan.SetActive(false);
+    }
+
+    public void DownloadMarker()
+    {
+        Application.OpenURL("https://drive.google.com/open?id=1wI6qURq9yIXv_aOrlN2G2JSO1mDaPy4a");
+    }
+
+
+}
